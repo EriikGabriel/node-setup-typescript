aef264fd57c0175c435da9e47c432f5df52885b8
EriikGabriel <erikgabriel.lins@hotmail.com>
2021-10-15 14:36:30 -0300

    Add Babel and exclude non-standard files

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from "express";
-import "@controllers/UsersController";
 
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+  return response.send("Hello World!!");
 });
 
 app.listen(3000, () => console.log("Server started at port 3000!"));
--- src/tests/firstTest.spec.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "@models/User";
-
-test("it should be okay", () => {
-  const user = new User();
-  user.name = "Erik";
-  expect(user.name).toEqual("Erik");
-});
